fbce37fd46be9fa46f9cef7fed08a7eebafdd2a4
Đoàn Văn Linh <linhdv71@ghtk.co>
2023-02-23 16:04:14 +0700

    wip

--- validators/category.js
+++ validators/category.js
@@ -1,16 +1,6 @@
 const {checkSchema} = require("express-validator")
 
-const validateCreateCategory = checkSchema({
-  category_type: {
-    in: 'body',
-    errorMessage: 'Category type must be filled',
-    trim: true,
-    isLength: {
-      options: {
-        min: 1
-      }
-    }
-  },
+const validateCreateCategory = checkSchema({ 
   category_name: {
     in: 'body',
     errorMessage: 'Category name must be filled',
