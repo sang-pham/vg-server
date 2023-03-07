cb8279767e7acf5d8027e7a4747aeebcaf0a2be7
linhdv71 <linhdv71@ghtk.co>
2023-03-07 10:33:28 +0700

    wip

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(cors())
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(passport.initialize());
 
 app.get(
